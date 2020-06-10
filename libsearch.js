3f5478451476e875186439dc87c83cfab6cfeaba
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-10 16:50:15 +0100

    Add files via upload

---
+++ libsearch.js
@@ -0,0 +1,18 @@
+    <script> 
+          
+        // Get HTML head element 
+        var head = document.getElementsByTagName('HEAD')[0];  
+  
+        // Create new link Element 
+        var link = document.createElement('link'); 
+  
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = '//cloud.webtype.com/css/2dc77ba2-6663-49b6-9e6c-aa009c06648b.css';  
+  
+        // Append link element to HTML head 
+        head.appendChild(link);  
+    </script>  
