868223b627293d42fc195524065d51baf9429250
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-15 16:10:54 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -13,3 +13,19 @@
   
         // Append link element to HTML head 
         document.head.appendChild(link);  
+
+  // Get HTML head element 
+        var footer = document.getElementsByTagName('footer')[0];  
+  
+        // Create new link Element 
+        var footlink = document.createElement('link'); 
+  
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/javascript'; 
+      
+        link.href = 'https://iainrca.github.io/summon/rta.js';  
+  
+        // Append link element to HTML head 
+        document.footer.appendChild(footlink);  
