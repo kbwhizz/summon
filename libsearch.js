08606893868d2409fdd92823530042c633c87a33
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-15 16:43:26 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -29,6 +29,31 @@ $('div.availabilityRta a').each(function () {
 });
 console.log("End of js file");
 
+const loadScript = (source, beforeEl, async = true, defer = true) => {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script');
+    const prior = beforeEl || document.getElementsByTagName('script')[0];
+
+    script.async = async;
+    script.defer = defer;
+
+    function onloadHander(_, isAbort) {
+      if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+        script.onload = null;
+        script.onreadystatechange = null;
+        script = undefined;
+
+        if (isAbort) { reject(); } else { resolve(); }
+      }
+    }
+
+    script.onload = onloadHander;
+    script.onreadystatechange = onloadHander;
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+  });
+}
 
 const scriptUrl = 'https://iainrca.github.io/summon/rta.js';
 loadScript(scriptUrl).then(() => {
