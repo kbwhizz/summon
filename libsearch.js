a82229b6e9301a5bae4bf3998cbef64b90a5d891
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-15 16:31:21 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -14,6 +14,21 @@
         // Append link element to HTML head 
         document.head.appendChild(link);
 
+        // Get HTML body element 
+        var footer = document.getElementsByTagName('body');  
+  
+        // Create new link Element 
+        var footlink = document.createElement('script'); 
+  
+        // set the attributes for script element  
+      
+        script.type = 'text/javascript'; 
+      
+        script.src = 'https://iainrca.github.io/summon/rta.js';  
+  
+        // Append link element to HTML head 
+        document.footer.appendChild(footlink);  
+
 $('div.availabilityRta a').each(function () {
    console.log($(this).text() );  
    var LocArr = $(this).text().split(',');
