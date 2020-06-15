5edd9f5c31b13853659188269adcb25777f5621c
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-15 16:08:20 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -13,16 +13,3 @@
   
         // Append link element to HTML head 
         document.head.appendChild(link);  
-
-console.log("Start of RTA function");
-
-$('div.availabilityRta a').each(function () {
-   console.log($(this).text() );  
-   var LocArr = $(this).text().split(',');
-         console.log(LocArr);
-   var Loc = LocArr[0];
-   if (Loc == 'CRL') {
-      console.log("CRL found");
- $( "<em>Some text</em></br>" ).insertBefore( $(this) );
-   }
-});
