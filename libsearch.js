b0a38fbcb92f43b45810e13147d66d34842f47cc
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-16 16:37:39 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -30,7 +30,7 @@
 //console.log("End of js file");
 
 $(document).ready(function() {
-    $('div.availabilityRta').load(function() {
+    $('div.availabilityRta').on('load', function() {
        if($('div.availabilities').length > 0) {
      console.log("RTA loaded");
 }
