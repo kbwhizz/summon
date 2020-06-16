d29f8ca9f5c5ca15b5548e76b263712b0019a5de
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-16 15:51:36 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -29,3 +29,13 @@ $(window).load($('div.availabilityRta a').each(function () {
 })
                   );
 console.log("End of js file");
+
+
+var block_to_insert ;
+var container_block ;
+ 
+block_to_insert = document.createElement( 'div' );
+block_to_insert.innerHTML = 'This demo DIV block was inserted into the page using JavaScript.' ;
+ 
+container_block = document.getElementById( 'footer' );
+container_block.appendChild( block_to_insert );
